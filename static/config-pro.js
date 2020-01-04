4f90a61f96f1aa0108503bfdeb32d193173c6c76
QQ853695231 <853695231@qq.com>
2020-01-04 22:50:06 +0800

    Initial commit

---
+++ static/config-pro.js
@@ -0,0 +1,15 @@
+var globalConfig = {
+  servers: {
+    url: 'http://27.128.187.131:9099',
+    fileServerApi: 'http://27.128.187.131:9604/',
+    plyrIconUrl: 'http://192.168.43.122:10100/images/20190829/_20190429110849_8675.svg'
+  },
+  activemq: {
+    ip: '192.168.43.101',
+    port: 61614,
+    user: 'admin',
+    password: 'admin',
+    topic: 'notice_server'
+  }
+};
+
---
+++ static/config.js
@@ -0,0 +1,15 @@
+var globalConfig = {
+  servers: {
+    url: 'http://192.168.31.5:5000',
+    fileServerApi: 'http://192.168.31.5:5000',
+    plyrIconUrl: 'http://192.168.43.122:10100/images/20190829/_20190429110849_8675.svg'
+  },
+  activemq: {
+    ip: '192.168.43.101',
+    port: 61614,
+    user: 'admin',
+    password: 'admin',
+    topic: 'notice_server'
+  }
+};
+
